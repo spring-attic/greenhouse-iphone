3f6c5808bb6bf2d8258b30d933705a526b852101
Roy Clarkson <rclarkson@vmware.com>
2010-06-22 18:13:19 -0400

    Added GreenhouseTests target for performing application tests, and LogicTests for performing logic tests. Now I just need to add some test cases.

---
+++ source/LogicTestSuite1.h
@@ -0,0 +1,18 @@
+//
+//  LogicTestSuite1.h
+//  Greenhouse
+//
+//  Created by Roy Clarkson on 6/22/10.
+//  Copyright 2010 VMware, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+
+@interface LogicTestSuite1 : SenTestCase 
+{
+
+}
+
+@end
