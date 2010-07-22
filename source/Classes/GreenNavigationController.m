be5dbd4a3616147abcd818069a6bc7db3daa2198
Roy Clarkson <rclarkson@vmware.com>
2010-07-22 16:45:39 -0400

    Made the navigation bar at the top green

---
+++ source/Classes/GreenNavigationController.m
@@ -0,0 +1,25 @@
+//
+//  GreenNavigationController.m
+//  Greenhouse
+//
+//  Created by Roy Clarkson on 7/22/10.
+//  Copyright 2010 VMware, Inc. All rights reserved.
+//
+
+#import "GreenNavigationController.h"
+
+
+@implementation GreenNavigationController
+
+
+#pragma mark -
+#pragma mark UIViewController methods
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+	
+	self.navigationBar.tintColor = [UIColor springDarkGreen];
+}
+
+@end
